2e7bd9a43f069c150552d1a2043ac9f610a1f224
Martin Fröhlich <martin.froehlich@quibiq.de>
2026-02-26 12:47:09 +0100

    Feature/map Daten Struktur erstellt
    * map eingebunden
    
    * Data Struktur
    
    * fix

---
+++ wwwroot/map.js
@@ -0,0 +1,11 @@
+﻿function initMap(lat, lon) {
+    const map = L.map('map').setView([lat, lon], 13);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '© OpenStreetMap contributors'
+    }).addTo(map);
+
+    L.marker([lat, lon]).addTo(map)
+        .bindPopup(`Lat: ${lat}<br>Lon: ${lon}`)
+        .openPopup();
+}
